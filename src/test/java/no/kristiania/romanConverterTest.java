907140f6b2c2bafe5edc53ad323a40596adbf106
theTurtl <addimanni@gmail.com>
2021-09-13 20:07:48 +0200

    Added a more universal method, which is more dynamic when converting from one to 10. void convert19ToXIX() failed.

--- src/test/java/no/kristiania/romanConverterTest.java
+++ src/test/java/no/kristiania/romanConverterTest.java
@@ -41,19 +41,46 @@ public class romanConverterTest {
         assertEquals("X", toRoman(10));
     }
 
+    @Test
+    void convert13ToXIII() {
+        assertEquals("XIII", toRoman(13));
+    }
+
+    @Test
+    void convert19ToXIX() {
+        assertEquals("XIX", toRoman(19));
+    }
+
     private Object toRoman(int number) {
         String result = "";
-        if(number <= 3) {
-            for (int i = 0; i < number; i++) {
-                result += "I";
+        int input = number;
+
+        while(input > 0){
+            if(input <= 3) {
+                for (int i = 0; i < input; i++) {
+                    result += "I";
+                    input -= 1;
+                }
             }
-        }else if (number == 4) {
-            result = "IV";
-        }else if (number == 5) {
-            result = "V";
-        }else if (number == 9) {
-            result = "IX";
+            if (number == 4) {
+                result = "IV";
+                input -= 4;
+            }
+            if (number == 5) {
+                result = "V";
+                input -= 5;
+            }
+            if (number == 9) {
+                result += "IX";
+                input -= 9;
+            }
+
+            if (input >= 10) {
+                result += "X";
+                input -= 10;
+            }
         }
+
         return result;
     }
 

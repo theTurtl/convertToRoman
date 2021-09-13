00a117c03b376b2d0489d56618e4deb600a024e0
theTurtl <addimanni@gmail.com>
2021-09-13 18:49:14 +0200

    updated Java version to 11, added test class.

---
+++ src/test/java/no/kristiania/romanConverterTest.java
@@ -0,0 +1,15 @@
+package no.kristiania;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class romanConverterTest {
+    @Test
+    void convertOneToI() {
+        assertEquals("I", toRoman(1));
+    }
+
+    private Object toRoman(int i) {
+    }
+}

ce2a9d18fb432fd3f652bd3964380515c9cd04f9
theTurtl <addimanni@gmail.com>
2021-09-14 13:02:29 +0200

    Solved void convert3999ToMMMCMXCIX(). Added code to manage conversion of 90 and 900 numbers. Refactored the code when input is under 10.

--- src/test/java/no/kristiania/romanConverterTest.java
+++ src/test/java/no/kristiania/romanConverterTest.java
@@ -86,49 +86,67 @@ public class romanConverterTest {
         assertEquals("MMMCMXCIX", toRoman(3999));
     }
 
+    @Test
+    void convert3898ToMMMDCCCXCVIII() {
+        assertEquals("MMMDCCCXCVIII", toRoman(3898));
+    }
+
+
 
     private Object toRoman(int number) {
         String result = "";
         int input = number;
 
         while(input > 0){
-            if(input <= 3) {
-                for (int i = 0; i < input; i++) {
-                    result += "I";
-                    input -= 1;
+
+
+            if (input < 10) {
+                if(input <= 3) {
+                    for (int i = 0; i < input; i++) {
+                        result += "I";
+                        input -= 1;
+                    }
+                }else if (input == 4) {
+                    result += "IV";
+                    input -= 4;
+                } else if (input >= 5 && input < 9) {
+                    result += "V";
+                    input -= 5;
+                } else if (input == 9) {
+                    result += "IX";
+                    input -= 9;
                 }
             }
-            if (input == 4) {
-                result += "IV";
-                input -= 4;
-            }
-            if (input >= 5 && input < 9) {
-                result += "V";
-                input -= 5;
-            }
-            if (input == 9) {
-                result += "IX";
-                input -= 9;
-            }
+
 
             if (input >= 10 && input < 50) {
                 result += "X";
                 input -= 10;
             }
 
-            if (input >= 50 && input < 100) {
+            if (input >= 50 && input < 90) {
                 result += "L";
                 input -= 50;
             }
+            if (input >= 90 && input < 100) {
+                result += "XC";
+                input -= 90;
+            }
+
 
             if (input >= 100 && input < 500) {
                 result += "C";
                 input -= 100;
             }
 
-            if (input >= 500 && input < 1000) {
+            if (input >= 500 && input < 900) {
                 result += "D";
                 input -= 500;
+            }
+
+            if (input >= 900 && input < 1000) {
+                result += "CM";
+                input -= 900;
             }
 
             if (input >= 1000) {

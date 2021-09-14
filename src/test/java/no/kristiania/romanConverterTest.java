2f002d0de96d0e2a901dd26f4d0621c6bef79ac2
theTurtl <addimanni@gmail.com>
2021-09-14 13:55:32 +0200

    Solved void convert3999ToMMMCMXCIX(). Added code to manage conversion of 90 and 900 numbers. Refactored the code when input is under 10. Also erased some tests to coincide with the requirements given in the assignment.

--- src/test/java/no/kristiania/romanConverterTest.java
+++ src/test/java/no/kristiania/romanConverterTest.java
@@ -5,40 +5,10 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class romanConverterTest {
-    @Test
-    void convertOneToI() {
-        assertEquals("I", toRoman(1));
-    }
-
-    @Test
-    void convertTwoToII() {
-        assertEquals("II", toRoman(2));
-    }
 
     @Test
     void convertThreeToIII() {
         assertEquals("III", toRoman(3));
-    }
-    
-
-    @Test
-    void convertFourToIV() {
-        assertEquals("IV", toRoman(4));
-    }
-
-    @Test
-    void convertFiveToV() {
-        assertEquals("V", toRoman(5));
-    }
-
-    @Test
-    void convertNineToIX() {
-        assertEquals("IX", toRoman(9));
-    }
-
-    @Test
-    void convertTenToX() {
-        assertEquals("X", toRoman(10));
     }
 
     @Test
@@ -52,33 +22,13 @@ public class romanConverterTest {
     }
 
     @Test
-    void convert20ToXX() {
-        assertEquals("XX", toRoman(20));
-    }
-
-    @Test
-    void convert50ToL() {
-        assertEquals("L", toRoman(50));
-    }
-
-    @Test
     void convert58ToLVIII() {
         assertEquals("LVIII", toRoman(58));
     }
 
     @Test
-    void convert100ToC() {
-        assertEquals("C", toRoman(100));
-    }
-
-    @Test
-    void convert500ToD() {
-        assertEquals("D", toRoman(500));
-    }
-
-    @Test
-    void convert1000ToM() {
-        assertEquals("M", toRoman(1000));
+    void convert1234ToM() {
+        assertEquals("MCCXXXIV", toRoman(1234));
     }
 
     @Test
@@ -91,15 +41,12 @@ public class romanConverterTest {
         assertEquals("MMMDCCCXCVIII", toRoman(3898));
     }
 
-
-
     private Object toRoman(int number) {
+
         String result = "";
         int input = number;
 
         while(input > 0){
-
-
             if (input < 10) {
                 if(input <= 3) {
                     for (int i = 0; i < input; i++) {
@@ -117,13 +64,10 @@ public class romanConverterTest {
                     input -= 9;
                 }
             }
-
-
             if (input >= 10 && input < 50) {
                 result += "X";
                 input -= 10;
             }
-
             if (input >= 50 && input < 90) {
                 result += "L";
                 input -= 50;
@@ -132,30 +76,23 @@ public class romanConverterTest {
                 result += "XC";
                 input -= 90;
             }
-
-
             if (input >= 100 && input < 500) {
                 result += "C";
                 input -= 100;
             }
-
             if (input >= 500 && input < 900) {
                 result += "D";
                 input -= 500;
             }
-
             if (input >= 900 && input < 1000) {
                 result += "CM";
                 input -= 900;
             }
-
             if (input >= 1000) {
                 result += "M";
                 input -= 1000;
             }
         }
-
         return result;
     }
-
 }
